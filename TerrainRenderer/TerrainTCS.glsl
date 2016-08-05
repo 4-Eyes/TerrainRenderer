3cc7fa373e22788e537409a50394375376993fcb
James Fairbairn <jamesaf112@gmail.com>
2016-08-05 17:52:20 +1200

    Adaptive tesselation now works properly

--- TerrainRenderer/TerrainTCS.glsl
+++ TerrainRenderer/TerrainTCS.glsl
@@ -7,12 +7,22 @@ uniform vec3 eyePos;
 void main()
 {
 	gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
-
-	/*float high = 6;
-	float low = 1;
-	float dmin = 0;
-	float dmax = 300;
-	float d = min(abs(distance(gl_in[gl_InvocationID].gl_Position.xyz, eyePos)), dmax);
+	
+	float d;
+	float high = 10;
+	float low = 2;
+	float dmin = abs(distance(gl_in[0].gl_Position.xyz, eyePos));
+	float dmax = dmin;
+	for (int i = 1; i < gl_in.length(); i++) {
+		d = abs(distance(gl_in[i].gl_Position.xyz, eyePos));
+		if (d < dmin) {
+			dmin = d;
+		}
+		else if (d > dmax) {
+			dmax = d;
+		}
+	}
+	d = abs(distance(gl_in[gl_InvocationID].gl_Position.xyz, eyePos));
 
 	highp int L = int(((d - dmin)/(dmax - dmin)) * (low - high) + high);
 	
@@ -20,14 +30,16 @@ void main()
 	gl_TessLevelOuter[1] = L;
 	gl_TessLevelOuter[2] = L;
 	gl_TessLevelOuter[3] = L;
-	gl_TessLevelInner[0] = L;
-	gl_TessLevelInner[1] = L;
+	gl_TessLevelInner[0] = L / 2;
+	gl_TessLevelInner[1] = L / 2;
+	
+	/*
+	gl_TessLevelOuter[0] = 4;
+	gl_TessLevelOuter[1] = 4;
+	gl_TessLevelOuter[2] = 4;
+	gl_TessLevelOuter[3] = 4;
+	gl_TessLevelInner[0] = 2;
+	gl_TessLevelInner[1] = 2;
 	*/
-	gl_TessLevelOuter[0] = 6;
-	gl_TessLevelOuter[1] = 6;
-	gl_TessLevelOuter[2] = 6;
-	gl_TessLevelOuter[3] = 6;
-	gl_TessLevelInner[0] = 3;
-	gl_TessLevelInner[1] = 3;
 	
 }
